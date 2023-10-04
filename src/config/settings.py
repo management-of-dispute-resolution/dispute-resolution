3ae646885c05ec58c12faf44ef896358968ec4c5
Ефимова Екатерина <MrKlejton@gmail.com>
2023-10-04 19:08:53 +0300

    A user model has been created, registered in the admin

--- src/config/settings.py
+++ src/config/settings.py
@@ -123,3 +123,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+USER_FIELD = 100
+
+# User
+
+AUTH_USER_MODEL = 'users.CustomUser'
--- src/users/models.py
+++ src/users/models.py
@@ -1,3 +1,77 @@
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 from django.db import models
 
-# Create your models here.
+from config.settings import USER_FIELD
+
+
+class CustomUser(AbstractUser):
+    USER = 'user'
+    PSICHOLOG = 'psicholog'
+    ADMINISTRATOR = 'admin'
+
+    USER_ROLES = [
+        (USER, 'user'),
+        (PSICHOLOG, 'psicholog'),
+        (ADMINISTRATOR, 'admin'),
+    ]
+    username = models.CharField(
+        max_length=USER_FIELD,
+        unique=True,
+        blank=False,
+        null=False,
+        verbose_name='Имя на сайте',
+        validators=[RegexValidator(
+            regex=r'^[\w.@+-]+$',
+            message='Имя пользователя содержит недопустимый символ'
+        )]
+    )
+    email = models.EmailField(
+        max_length=USER_FIELD,
+        unique=True,
+        blank=False,
+        null=False,
+        verbose_name='Адрес электронной почты'
+    )
+    first_name = models.CharField(
+        verbose_name='Имя',
+        max_length=USER_FIELD,
+        blank=True
+    )
+    last_name = models.CharField(
+        verbose_name='Фамилия',
+        max_length=USER_FIELD,
+        blank=True
+    )
+    image = models.ImageField(
+        # upload_to='users_images',
+        blank=True,
+        null=True,
+        verbose_name='Фото'
+    )
+    role = models.CharField(
+        verbose_name='Роль',
+        max_length=USER_FIELD,
+        choices=USER_ROLES,
+        default=USER
+    )
+
+    @property
+    def is_user(self):
+        return self.role == 'user'
+
+    @property
+    def is_admin(self):
+        return self.role == 'admin'
+
+    @property
+    def is_psicholog(self):
+        return self.role == 'psicholog'
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        ordering = ('username')
+        verbose_name = 'Участник сообщества'
+        verbose_name_plural = 'Участники сообщества'
