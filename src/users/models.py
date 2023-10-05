d7a99d92b017bf0736cc811d62f8354dd408b286
Kate <116462786+MrKlejton@users.noreply.github.com>
2023-10-05 22:53:43 +0300

    A user model has been created, registered in (#1)
    * A user model has been created, registered in
    the admin
    
    * Add fields post, department. Fixed  roles
    
    ---------
    
    Co-authored-by: Ефимова Екатерина <MrKlejton@gmail.com>

--- src/users/models.py
+++ src/users/models.py
@@ -1,3 +1,87 @@
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 from django.db import models
 
-# Create your models here.
+from config.settings import USER_FIELD
+
+
+class CustomUser(AbstractUser):
+    USER = 'user'
+    PSICHOLOGIST = 'psichologist'
+    ADMINISTRATOR = 'admin'
+
+    USER_ROLES = [
+        (USER, 'user'),
+        (PSICHOLOGIST, 'psichologist'),
+        (ADMINISTRATOR, 'admin'),
+    ]
+    username = models.CharField(
+        max_length=USER_FIELD,
+        unique=True,
+        blank=False,
+        null=False,
+        verbose_name='Имя на сайте',
+        validators=[RegexValidator(
+            regex=r'^[\w.@+-]+$',
+            message='Имя пользователя содержит недопустимый символ'
+        )]
+    )
+    email = models.EmailField(
+        max_length=USER_FIELD,
+        unique=True,
+        blank=False,
+        null=False,
+        verbose_name='Адрес электронной почты'
+    )
+    first_name = models.CharField(
+        verbose_name='Имя',
+        max_length=USER_FIELD,
+        blank=True
+    )
+    last_name = models.CharField(
+        verbose_name='Фамилия',
+        max_length=USER_FIELD,
+        blank=True
+    )
+    department = models.CharField(
+        verbose_name='Отдел',
+        max_length=USER_FIELD
+    )
+    post = models.CharField(
+        blank=False,
+        null=False,
+        verbose_name='Должность',
+        max_length=USER_FIELD
+    )
+    image = models.ImageField(
+        # upload_to='users_images',
+        blank=True,
+        null=True,
+        verbose_name='Фото'
+    )
+    role = models.CharField(
+        verbose_name='Роль',
+        max_length=USER_FIELD,
+        choices=USER_ROLES,
+        default=USER
+    )
+
+    @property
+    def is_user(self):
+        return self.role == 'user'
+
+    @property
+    def is_admin(self):
+        return self.role == 'admin'
+
+    @property
+    def is_psichologist(self):
+        return self.role == 'psichologist'
+
+    class Meta:
+        ordering = ('username')
+        verbose_name = 'Участник сообщества'
+        verbose_name_plural = 'Участники сообщества'
+
+    def __str__(self):
+        return self.username
