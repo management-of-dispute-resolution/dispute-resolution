c1874054060d2fc7c1690aa5fa44ca173fb58a11
Maxim <119324137+zxcmax@users.noreply.github.com>
2023-10-07 21:29:39 +0700

    Created initial models for disputes (#11)
    * Created initial models for disputes
    
    * Added abstract BaseModel with created_at field
    
    * Fix

--- src/disputes/admin.py
+++ src/disputes/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Dispute, DisputeParticipants, Message, Attachment
+
+admin.site.register(Dispute)
+admin.site.register(DisputeParticipants)
+admin.site.register(Message)
+admin.site.register(Attachment)
