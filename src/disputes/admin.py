d615a7af6bc8db4adc745df1b2fa1d72484ccb48
Aleksei Koznov <alekseikoznov@yandex.ru>
2023-10-12 11:16:57 +0300

    change models in apps Users and Disputes' (#18)

--- src/disputes/admin.py
+++ src/disputes/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 
-from .models import Dispute, DisputeParticipants, Message, Attachment
+from .models import Comment, CommentDispute, Dispute
 
 admin.site.register(Dispute)
-admin.site.register(DisputeParticipants)
-admin.site.register(Message)
-admin.site.register(Attachment)
+admin.site.register(Comment)
+admin.site.register(CommentDispute)
