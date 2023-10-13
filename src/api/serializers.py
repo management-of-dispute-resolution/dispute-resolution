bed9f16d57707a8edec896d552179fc82787ab95
Ольга Шполянская <olgab497@gmail.com>
2023-10-13 16:28:36 +0300

    CommentSerializer added

--- src/api/serializers.py
+++ src/api/serializers.py
@@ -1,17 +1,56 @@
 from djoser.serializers import UserSerializer
 from rest_framework import serializers
 
-from disputes.models import Dispute
+from disputes.models import Comment, Dispute
 from users.models import CustomUser
 
 
 class CustomUserSerializer(UserSerializer):
+    """Serializer for the CustomUser model."""
+
     class Meta:
+        """
+        Meta class CustomUserSerializer.
+
+        Attributes:
+            model: The CustomUser model class to be serialized.
+            fields: A string indicating to include all fields
+            from the CustomUser model.
+        """
+
         model = CustomUser
         fields = '__all__'
 
 
 class DisputeSerializer(serializers.ModelSerializer):
+    """Serializer for the Dispute model."""
+
     class Meta:
+        """
+        Meta class DisputetSerializer.
+
+        Attributes:
+            model: The Dispute model class to be serialized.
+            fields: A string indicating to include all fields
+            from the Dispute model.
+        """
+
         model = Dispute
+        fields = '__all__'
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    """Serializer for the Comment model."""
+
+    class Meta:
+        """
+        Meta class for CommentSerializer.
+
+        Attributes:
+            model: The Comment model class to be serialized.
+            fields: A string indicating to include all fields
+            from the Comment model.
+        """
+
+        model = Comment
         fields = '__all__'
