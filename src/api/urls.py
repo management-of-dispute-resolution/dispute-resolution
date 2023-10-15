6adf8a4c66439994458589bf7207601232345d48
Ольга Шполянская <olgab497@gmail.com>
2023-10-15 18:11:15 +0300

    url settings for swagger/redoc added

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,4 +1,7 @@
-from django.urls import include, path
+from django.urls import include, path, re_path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 from rest_framework.routers import DefaultRouter
 
 from .views import (
@@ -16,11 +19,39 @@ router.register('users', CustomUserViewSet, basename='users')
 router.register('disputes', DisputeViewSet)
 router.register('comments', CommentViewSet)
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Snippets API",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     path(
         'api/comments/<int:pk>/',
-        CommentDetailViewSet.as_view(),
+        CommentDetailViewSet.as_view({'get': 'retrieve', 'delete': 'destroy'}),
         name='comment-detail',
+    ),
+    re_path(
+        r'^swagger(?P<format>\.json|\.yaml)$',
+        schema_view.without_ui(cache_timeout=0),
+        name='schema-json',
+    ),
+    path(
+        'swagger/',
+        schema_view.with_ui('swagger', cache_timeout=0),
+        name='schema-swagger-ui',
+    ),
+    path(
+        'redoc/',
+        schema_view.with_ui('redoc', cache_timeout=0),
+        name='schema-redoc',
     ),
     path('', include(router.urls)),
     path('', include('djoser.urls')),
