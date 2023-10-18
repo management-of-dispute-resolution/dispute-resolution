9317f8062832ad9153bd510614991704326b27dc
Ольга Шполянская <olgab497@gmail.com>
2023-10-18 16:15:56 +0300

    PreventRegistrationMiddleware added

--- src/config/settings.py
+++ src/config/settings.py
@@ -41,6 +41,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'api.custom_middleware.PreventRegistrationMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
