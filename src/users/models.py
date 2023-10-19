f7d3a88bb85fb065ac11e3347e19cbd00e0c5b03
Ольга Шполянская <olgab497@gmail.com>
2023-10-19 14:20:46 +0300

    CustomUserManager added for

--- src/users/models.py
+++ src/users/models.py
@@ -1,8 +1,57 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.core.validators import RegexValidator
 from django.db import models
 
 from config.settings import USER_FIELD
+
+
+class CustomUserManager(BaseUserManager):
+    """
+    Custom manager for the CustomUser model.
+
+    This manager provides custom methods
+    for creating regular users and superusers.
+    """
+
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        """
+        Private method.
+
+        Creates and saves a CustomUser
+        with the given email and password.
+        """
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password, **extra_fields):
+        """
+        Create a regular user with the given email and password.
+
+        Uses _create_user.
+        """
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        """
+        Create a superuser with the given email and password.
+
+        Uses _create_user.
+        """
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
 
 
 class CustomUser(AbstractUser):
@@ -28,7 +77,10 @@ class CustomUser(AbstractUser):
     USERNAME_FIELD = 'email'
 
     # Update of REQUIRED_FIELDS
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ['password']
+
+    # Connects CustomUser model with CustomUserManager
+    objects = CustomUserManager()
 
     first_name = models.CharField(
         verbose_name='Имя', max_length=USER_FIELD, blank=True
