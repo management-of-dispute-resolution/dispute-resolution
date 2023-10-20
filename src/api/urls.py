205ffa0afa70f41706971d15eaa9831c7343f57a
Ольга Шполянская <olgab497@gmail.com>
2023-10-20 16:16:36 +0300

    missing djoser paths added

--- src/api/urls.py
+++ src/api/urls.py
@@ -46,4 +46,6 @@ urlpatterns = [
          cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc',
          cache_timeout=0), name='schema-redoc'),
+    path("auth/", include("djoser.urls")),
+    re_path(r'^auth/', include('djoser.urls.authtoken')),
 ]
