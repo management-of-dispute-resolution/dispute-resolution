c90333bced2c8ccc64dc01f770fca763205672ec
Анастасия Савельева <Visteria09@yandex.ru>
2023-11-10 20:47:27 +0700

    проверка прекоммита

--- src/api/views.py
+++ src/api/views.py
@@ -13,8 +13,7 @@ from api.serializers import (
     CommentSerializer,
     CustomUserSerializer,
     DisputeSerializer,
-    PatchDisputeSerializer,
-)
+    PatchDisputeSerializer,)
 from disputes.models import Comment, Dispute
 from users.models import CustomUser
 
