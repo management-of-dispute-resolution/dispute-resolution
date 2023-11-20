e476f9e96fe975be5fd9aee035a506efb526752f
Анастасия Савельева <Visteria09@yandex.ru>
2023-11-20 22:56:01 +0700

    вынесен вылидатор из модели юзер и добавлены миграции

--- src/users/models.py
+++ src/users/models.py
@@ -3,10 +3,10 @@ from django.contrib.auth.models import (
     BaseUserManager,
     PermissionsMixin,
 )
-from django.core.validators import RegexValidator
 from django.db import models
 
 from config.settings import USER_FIELD
+from users.validators import phone_number_validator
 
 
 class CustomUserManager(BaseUserManager):
@@ -137,14 +137,7 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     phone_number = models.CharField(
         max_length=16,
         unique=True,
-        validators=[
-            RegexValidator(
-                regex=r'''^(\+7|7|8)?[\s\-]?\(?[489][0-9]{2}\)?[\s\-]?[0-9]{3}[
-                    \s\-]?[0-9]{2}[\s\-]?[0-9]{2}$''',
-                message='''Номер телефона может начинаться только с 7, +7, 8 и
-                разделители только "-" или пробел.''',
-            )
-        ],
+        validators=[phone_number_validator()],
     )
     role = models.CharField(
         verbose_name='Роль',
