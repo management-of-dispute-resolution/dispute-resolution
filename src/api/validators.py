361a94d376db521c01e026b106d17042c6658dbc
Ольга Шполянская <olgab497@gmail.com>
2023-11-21 13:32:08 +0300

    UniquePasswordValidator added

---
+++ src/api/validators.py
@@ -0,0 +1,47 @@
+from django.core.exceptions import ValidationError
+from django.core.validators import BaseValidator
+
+from api.api_consts import ERROR_MESSAGE
+
+
+class UniquePasswordValidator(BaseValidator):
+    """
+    Check the new password and the current password.
+
+    This validator checks if the new password
+    matches the current password of the user.
+    If they match, a ValidationError is raised.
+    """
+
+    def __init__(self, limit_value=1, *args, **kwargs):
+        """
+        Initialize the UniquePasswordValidator.
+
+        Args:
+            limit_value (int): The number of similar passwords
+            to check against (default is 1).
+        """
+        super().__init__(limit_value, *args, **kwargs)
+
+    def validate(self, password, user=None):
+        """
+        Validate the new password against the current password.
+
+        Args:
+            password (str): The new password to validate.
+            user: The user object associated with the password
+            (default is None).
+
+        Raises:
+            ValidationError: If the new password is
+            the same as the current password.
+        """
+        if user and user.check_password(password):
+            raise ValidationError(
+                (ERROR_MESSAGE),
+                code='password_reuse',
+            )
+
+    def get_help_text(self):
+        """Return a help text describing the validation rule."""
+        return (ERROR_MESSAGE)
