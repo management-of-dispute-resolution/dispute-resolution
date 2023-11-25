50ee5cf7040173e0a60ae9c2c93cf0206c2bec1e
Aleksei Koznov <alekseikoznov@yandex.ru>
2023-11-25 19:03:15 +0300

    change regex (#159)

--- src/disputes/validators.py
+++ src/disputes/validators.py
@@ -4,6 +4,6 @@ from django.core.validators import RegexValidator
 def text_validator():
     """Check the text."""
     return RegexValidator(
-        regex=r'''^[A-Za-zа-яА-Я0-9_\s-]*$''',
+        regex=r'^[A-Za-zА-Яа-я0-9\s\W]+$',
         message='''Текст может быть только на латинице и кириллице''',
     )
